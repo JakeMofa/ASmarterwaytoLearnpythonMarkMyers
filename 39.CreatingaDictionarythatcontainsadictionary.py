99ca618adb1ede5bf98fc14320a8bcd8c5d17d85
Jake Mofa <97313664+JakeMofa@users.noreply.github.com>
2025-04-24 08:52:54 -0500

    39-40

--- 39.CreatingaDictionarythatcontainsadictionary.py
+++ 39.CreatingaDictionarythatcontainsadictionary.py
@@ -1,21 +1,34 @@
-customers = [ {
+customers = { 
+             0: {
      "customer id": 0,
      "first name":"John",
      "last name": "Ogden",
     "address": "301 Arbor Rd.",  
     },
-    {
+    1:{
      "customer id": 1,
     "first name":"Ann",
     "last name": "Sattermyer",
     "address": "PO Box 1145",
     },  
-    {
+    2:{
     "customer id": 2,
     "first name":"Jill",
     "last name": "Somers",
     "address": "3 Main St.",
     },
-]
+}
 
+
+sorry = {
+    "customer_id" : 1,
+    "First_name" : 2,
+    "Last_name" : 3,
+}
+
+customers[3] = sorry
+print(customers)
+del customers[3]
+print(customers)
 ##creating a dictioninary inside a dictionary and replacing these with lists instead
+## we can also use names for the placement holders for the
---
+++ 40.test.py
@@ -0,0 +1,12 @@
+customer_29876 = {"first name": "David", "last name": "Elliott", "address": "4803 Wellesley St."}
+
+keys = []
+values = []
+
+for key, value in customer_29876.items():
+    keys.append(key)
+    values.append(value)
+    
+print(f"This is the key:, {keys}")
+print(f"this is the value:,  {values}")
+
